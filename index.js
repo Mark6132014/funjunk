5895ce6fdab562135f977fe9c8b3b9b535109cbc
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-09-12 18:21:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ setTimeout(() => {
     document.querySelector(".intro").style.opacity = 0;
     setTimeout(hideOptions, 950);
 }, 3000);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then((reg) => {
+        console.log('Service worker registered:', reg);
+      })
+      .catch((error) => {
+        console.error('Service worker registration failed:', error);
+      });
+  });
+}
